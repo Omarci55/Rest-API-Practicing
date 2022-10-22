1303d446bbb18f9b02e5570eed2b0def462b165f
o.marci@gmx.de <o.marci@gmx.de>
2022-10-22 14:54:19 +0200

    Update new changes

---
+++ data/books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        "id": "13",
+        "title": "E foi assim",
+        "year": "2019",
+        "stock": "true"
+    },
+    {
+        "id": "26",
+        "title": "Ou foi daquele jeito",
+        "year": "2020",
+        "stock": "true"
+    },
+    {
+        "id": "39",
+        "title": "Nem sei mais",
+        "year": "2021",
+        "stock": "false"
+    }
+]
